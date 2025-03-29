be196577c36c4b7e9a6be61a1ccad0782872314f
MD SAYEDUL HAQUE <takisayedul@gmail.com>
2025-03-29 13:42:37 +0600

    Add: initialization

---
+++ src/app.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigModuleOptions } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    } as ConfigModuleOptions), // 👈 Explicitly cast to avoid type issues
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
+})
+export class AppModule {}
